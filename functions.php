2a7c4a749c0368b234834d536b6e986565c75384
luciussulla <maciek.kowalczyk@yandex.com>
2021-12-27 10:41:08 +0100

    refactor split_question function

--- functions.php
+++ functions.php
@@ -23,35 +23,17 @@
 
   function split_question($question_assoc) {
     // split question uses the build_question function
-
-    $is_match = preg_match("/__/", $question_assoc["question"]); 
+    $question_from_db = $question_assoc["question"]; 
+    $is_match = preg_match("/__/", $question_from_db); // quesion in db has __ where the user shuld privide his answer (blank space for user to insert answer)
     if ($is_match) {
-      $array =  preg_split("/__/", $question_assoc["question"]);
-      $question_input = build_question($array, $question_assoc["id"]); // the question is built and the input is inserted instead of "__";
-      return $question_input; 
+      $array =  preg_split("/__/", $question_from_db);
+      $question_with_input = build_question($array, $question_assoc["id"]); // the question is built and the input field is inserted instead of "__";
+      return $question_with_input; 
     } else {
       // if no blank was inserted just the beginning of the question you get the question back
       // still need work here to return full question with input field
-      return $question_assoc["question"]; 
+      return $question_from_db;  
     } 
   } 
   
-  function correct_answers_into_json($query_result) {
-    global $connection; 
-    // var_dump($query_result); 
-    // echo $query_result; 
-
-    $question_answer_array = array(); 
-    while($assoc = mysqli_fetch_assoc($query_result)) {
-      var_dump($assoc); 
-      echo "dupa"; 
-      // echo $assoc; 
-      $question_answer_array[] = array('id'=>$assoc["id"], 'answer'=>$assoc["answer"]);
-      // array_push($question_answer_array, $obj); 
-    }
-    // mysqli_free_result($query_result); 
-    // var_dump($question_answer_array); 
-    return $question_answer_array; 
-  }
-
 ?>  
--- test/show.php
+++ test/show.php
@@ -26,21 +26,17 @@
 <body>
 
   <?php 
-    $answers_array = array(); // prepare JONS with correct answers and question_ids
+    $answers_array = array(); // prepare JONS with correct answers and question_ids which then is send as hidden field to show for checking users answers
     $form = "<form action=\"../checker/checker.php\" method=\"post\">"; 
     while($row = mysqli_fetch_assoc($result)) {
-      // lets get values from db (all questions for now)
-      // have a look at the values with var_dump($row); 
-      // need a function that takes all correct_answers and question_ids and pairs them into a $list = array(), use array_push($list, $obj)
-      // that list will be send as a hidden vield in a JSON format 
       $answers_array[] = array('id'=>$row['id'], 'answer'=>$row['answer']); 
-      // then the list will be compared in the checkour with the values answers provided by the user 
       // use a validation in the checker to see if the number of sent questions is the same as the number of anwers in the JSON array (make suer we are checking the right things)
       $ready_question = split_question($row);
       $form .= "<div>";
       $form .= $ready_question;   
       $form .= "</div>"; 
     }
+
     $answers_array_json = json_encode($answers_array); 
     var_dump($answers_array_json);
     $form .= "<input type=\"hidden\" name=\"answers_json\" value='" . $answers_array_json . "' >";
@@ -48,10 +44,7 @@
     $form .= "</form>"; 
     echo $form
   ?>
-
-  <?php
-    mysqli_free_result($result); 
-  ?>
+  <?php mysqli_free_result($result);?>
 
 </body>
 </html>
